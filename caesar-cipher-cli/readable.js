04f8d700fabb0f78eceb663cb4333855d4a7150e
Vitaliy <vetal.morenko@mail.ru>
2020-10-05 21:51:24 +0300

    feat: add task caesar-cipher-cli

---
+++ caesar-cipher-cli/readable.js
@@ -0,0 +1,24 @@
+const { errorHandler } = require('./writable');
+
+const readable = async ({ shift, action }) => {
+  if (isNaN(shift) && typeof shift === 'number') {
+    errorHandler('argument "shift" is not number', 8);
+  }
+
+  if (shift === undefined) {
+    errorHandler('argument "shift" is required', 8);
+  }
+
+  if (action === undefined) {
+    errorHandler('argument "action" is required', 8);
+  }
+
+  if (action !== 'encode' && action !== 'decode') {
+    errorHandler(
+      'argument "action" has not valid value ["encode"|"decode"]',
+      8
+    );
+  }
+};
+
+module.exports = readable;
